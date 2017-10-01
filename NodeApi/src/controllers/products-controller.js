8d756165568cd92de2390d61b9167b34e580c462
OsmarJunior <oj13black@gmail.com>
2017-10-01 12:10:13 -0300

    Add Methods put and delete

--- NodeApi/src/controllers/products-controller.js
+++ NodeApi/src/controllers/products-controller.js
@@ -33,7 +33,30 @@ exports.getBySlug = (req, res, next) => {
         });
 };
 
-exports.post = (req, res, next) => {
+exports.getById = (req, res, next) => {
+    Product
+        .findById(req.params.id)
+        .then(data => {
+            res.status(200).send(data);
+        }).catch(e =>{
+            res.status(400).send(e);
+        });
+}
+
+exports.getByTag = (req, res, next) =>{
+    Product
+        .find({
+            tags: req.params.tags
+        })
+        .then(data => {
+            res.status(200).send(data);
+        })
+        .catch(e => {
+            res.status(400).send(e);
+        });
+}
+
+exports.post = (req, res, next) => {''
     var product = new Product(req.body);
     //OU
     // var product = new Product();
@@ -54,13 +77,58 @@ exports.post = (req, res, next) => {
 };
 
 exports.put = (req, res, next) => {
-    let id = req.params.id;
-    res.status(200).send({
-        id: id,
-        item: req.body
-    });
-};
+    Product
+        .findByIdAndUpdate(req.params.id, {
+            $set: {
+                title: req.body.title,
+                description: req.body.description,
+                price: req.body.price
+            }
+        }).then(x => {
+            res.status(200).send({
+                message: 'Produto Atualizado com Sucesso!'
+            });
+        }).catch(e => {
+            res.status(400).send({
+                message: 'Falha ao atualizar Produto',
+                data: e
+            });
+        });
+}
+
+// exports.put = (req, res, next) => {
+//     let id = req.params.id;
+//     res.status(200).send({
+//         id: id,
+//         item: req.body
+//     });
+// };
 
 exports.delete = (req, res, next) => {
-    res.status(200).send(req.body);
-};
+    Product
+        .findOneAndRemove(req.body.id)
+        .then(x => {
+            res.status(200).send({
+                message: 'Produto removido com sucesso!'
+            });
+        }).catch(e => {
+            res.status(400).send({
+                message: 'Falha ao remover Produto',
+                data: e
+            });
+        });        
+}
+
+// exports.delete = (req, res, next) => {
+//     res.status(200).send(req.body);
+// };
+
+
+
+
+
+
+
+
+
+
--- NodeApi/src/routes/productsRoute.js
+++ NodeApi/src/routes/productsRoute.js
@@ -6,6 +6,8 @@ const controller = require('../controllers/products-controller');
 
 router.get('/', controller.get);
 router.get('/:slug', controller.getBySlug);
+router.get('/admin/:id', controller.getById);
+router.get('/tags/:tags', controller.getByTag);
 router.post('/', controller.post);
 router.put('/:id', controller.put);
 router.delete('/', controller.delete);
